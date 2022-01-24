5470e4824ff099ce648ed2eece9b23d4142cc8a4
Evan Decker <evan.decker@gmail.com>
2022-01-24 15:17:29 -0800

    Added a comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        // Commenting in Java appears to be the same as JavaScript. (New Change)
     }
 }

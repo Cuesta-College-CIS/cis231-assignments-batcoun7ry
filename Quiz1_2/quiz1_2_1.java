14edba30580a3f216595e5559c8b608cbf079471
Evan Decker <evan.decker@gmail.com>
2022-04-02 18:16:42 -0700

    added comments; minval & minidx not working

--- Quiz1_2/quiz1_2_1.java
+++ Quiz1_2/quiz1_2_1.java
@@ -1,3 +1,20 @@
+/*
+Question 1 : Find the i-th smallest number:
+
+This program will take 10 user inputs and construct an integer array
+Input begins with the number of elements 10 followed by 10 integer values
+10 20 35 25 10 15 40 55 50 0 45
+Swap the smallest value and the 1st array element
+Swap the 2nd smallest value and 2nd array element
+Swap the 3nd smallest value and 3rd array element
+Swap the 4nd smallest value and 4th array element
+Swap the 5nd smallest value and 5th array element
+All other values have to keep the original place
+
+Program Template Code
+Link : https://github.com/Kwooley/CIS231/blob/main/Quiz1/quiz1template.java 
+*/
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -17,20 +34,25 @@ public class quiz1_2_1 {
         }
 
         // print array for debugging:
-		System.out.println(Arrays.toString(numbers));
+	    System.out.println(Arrays.toString(numbers));
 
         // loop through array to find min element:
-        minval = numbers[0];
-        for (int i = 0; i < numbers.length; i++) {
+        minval = numbers[1];
+        minidx = numbers[1];
+        for (int i = 0; i < N; i++) {
             if (numbers[i] < minval) {
                 minval = numbers[i];
+                i = minidx; // save index of minval
             }
         }
 
-        //print minval for debugging:
-        System.out.println(minval);
+        // print minval & minidx for debugging:
+        System.out.println(
+            "minval: " + minval + "\n" +
+            "minidx: " + minidx
+        );
 
-        //close scanner:
+        // close scanner:
 		scnr.close();
 	}
 }

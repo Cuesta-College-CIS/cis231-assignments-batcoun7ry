0d367ab8de3b20fdb751149db4066a1d04a9a1ff
Evan Decker <evan.decker@gmail.com>
2022-04-02 17:28:02 -0700

    implemented working minval finder

--- Quiz1_2/quiz1_2_1.java
+++ Quiz1_2/quiz1_2_1.java
@@ -12,11 +12,23 @@ public class quiz1_2_1 {
 		Scanner scnr = new Scanner(System.in);
 		int N = scnr.nextInt();
 		int[] numbers = new int[N];
-		for (int i = 0; i < N; i++)
+		for (int i = 0; i < N; i++) {
 			numbers[i] = scnr.nextInt();
+        }
 
         // print array for debugging:
 		System.out.println(Arrays.toString(numbers));
+
+        // loop through array to find min element:
+        minval = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < minval) {
+                minval = numbers[i];
+            }
+        }
+
+        //print minval for debugging:
+        System.out.println(minval);
 
         //close scanner:
 		scnr.close();

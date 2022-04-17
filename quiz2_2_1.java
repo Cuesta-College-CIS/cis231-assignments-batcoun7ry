d8d760879fc77284cdae68362ae05409a7d57845
Evan Decker <evan.decker@gmail.com>
2022-04-17 16:18:31 -0700

    program start

---
+++ quiz2_2_1.java
@@ -0,0 +1,31 @@
+/*
+Make program that has 4 user-defined functions to:
+    // construct two integer arrays with random values (0-99), return the array
+    (1) public static int[] constructArray(Scanner scnr)
+
+    //print all elements in each array
+    (2) public static void printArray(int[])
+
+    //determine whcih array has more even numbers and return the array number
+    (3) public static int findMoreEvens(int[] n1, int[] n2)
+        // in main function, print the message
+        // oubput example: Array 2 has more even numbers
+    
+
+    // derermine which array has the grteatest value and return the array number
+    (4) public static int findGreatest(int[] n1, int[] n2)
+        // in main function, print the array number and the greatest value together
+        // output example: Array 1 has the greatest value 99
+*/
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class quiz2_2_1 {
+	public static void main(String[] args) {
+		//Scanner scnr = new Scanner(System.in);
+        System.out.print("test");
+
+	}
+}

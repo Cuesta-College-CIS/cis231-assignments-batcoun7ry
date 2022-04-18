d352c0a79a77befba2df3b4a21daf186fab4f6f6
Evan Decker <evan.decker@gmail.com>
2022-04-17 18:00:32 -0700

    compiles but doesnt get user's N for some reason

--- quiz2_2_1.java
+++ quiz2_2_1.java
@@ -27,33 +27,35 @@ import java.util.Random;
 public class quiz2_2_1 {
 
     // construct two integer arrays with random values (0-99), return the array
-    // take one user input N for the size of the array
+    // take user input (N) for the size of the array
     public static int[] constructArray(Scanner scnr) {
-        
-        Random randGen = new Random(); // make random number generator
 
-        int [] array = new int[scnr]; // make array with user's N elements
+        Random randGen = new Random(); // make random number generator
+        int N = scnr.nextInt(); // get user's N
+        int [] array = new int[N]; // make array with N elements
 
         //loop through array and populate each element with random number:
         for (int i = 0; i < array.length; i++) {
-            array[i] = randGen.nextInt(100)
+            array[i] = randGen.nextInt(100);
         }
+        return array;
     }
 
     //print all elements in each array
-    public static void printArray(int[]) {
-        System.out.println(Arrays.toString(array1);
+    public static void printArray(int[] array) {
+
+        System.out.println(Arrays.toString(array));
     }
 
     //determine which array has more even numbers and return the array number
-    public static int findMoreEvens(int[] n1, int[] n2) {
+    //public static int findMoreEvens(int[] n1, int[] n2) {
 
-    }
+    //}
 
     // derermine which array has the grteatest value and return the array number
-    public static int findGreatest(int[] n1, int[] n2) {
+    //public static int findGreatest(int[] n1, int[] n2) {
 
-    }
+    //}
 
 	public static void main(String[] args) {
 
@@ -63,7 +65,7 @@ public class quiz2_2_1 {
         array1 = constructArray(scnr);
 
         // print array1
-        array1 = constructArray();
+        //array1 = constructArray();
         printArray(array1);
 
 
